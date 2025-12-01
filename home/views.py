d508f5d28abf6796c74eca25d6b411451307ab8b
otaviof84 <otavioguto84@gmail.com>
2025-12-01 12:47:59 -0300

    y

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("A view index funcionou, wow!!")
+    return HttpResponse("Bem vindos ao nosso site")
 
 def sobre(request):
     return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim mesmo.</h1>")
