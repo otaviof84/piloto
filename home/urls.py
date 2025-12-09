b23411b43ffe50b3ccd742938370031156186161
otaviof84 <otavioguto84@gmail.com>
2025-12-09 15:47:32 -0300

    u

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('sobre/', views.sobre, name="sobre"),
     path('contato/', views.contato, name="contato"), 
-    path('ajuda/', views.ajuda, name="ajuda"), 
+    path('ajuda/', views.ajuda, name="ajuda"),
+    path('produtos/', views.produtos, name="produtos"),
+    path('item/<int:id>/', views.exibir_item, name='exibir_item'),
+    path('dia/<int:num>/', views.diasemana, name='diasemana'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
@@ -11,3 +12,31 @@ def contato(request):
 
 def ajuda(request):
     return render(request, 'ajuda.html')
+
+def produtos(request):
+    contexto = {
+        'lista': [
+            {'id': 1, 'nome': 'Notebook', 'preco': '2.500,00'},
+            {'id': 2, 'nome': 'Monitor', 'preco': '500,00'},
+            {'id': 3, 'nome': 'Teclado', 'preco': '80,00'},
+        ],
+    }
+    return render(request, 'produto/lista.html', contexto)
+
+def exibir_item(request, id):
+    return HttpResponse(f"Você pediu para exibir o item {id}")
+
+def diasemana(request, num):
+    dias = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado",
+    }
+
+    nome = dias.get(num, "Dia inválido")
+    return HttpResponse(f"Dia escolhido: {nome}")
+
